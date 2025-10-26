6922ca911c9afdc1c5fcdce24045376361f8f7a2
Antek <a.zajdel24@gmail.com>
2025-10-26 14:13:45 +0100

    w miarę gotowe do rozbudowy

--- game.py
+++ game.py
@@ -81,8 +81,7 @@ class Game:
 
             ready_peers = 0
             for known_peer in self.peer.known_peers.keys():
-                if self.peer.known_peers[known_peer] == 'True':
-                    test = self.peer.known_peers[known_peer]
+                if self.peer.known_peers[known_peer]:
                     ready_peers += 1
             if ready_peers == len(self.peer.known_peers) and ready_peers != 0 and self.peer.my_ready_status == True:
                 lobby_loop = False
