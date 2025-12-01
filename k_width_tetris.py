c412f3ea102cb49bc06c00706dabb21d1ecd6d3b
Antek <a.zajdel24@gmail.com>
2025-12-01 13:24:59 +0100

    poprawki k-width

--- k_width_tetris.py
+++ k_width_tetris.py
@@ -95,7 +95,7 @@ class KWidthTetris(g.Game):
 
             for event in pg.event.get():
                 if event.type == pg.QUIT:
-                    self.peer.send_msg_to_all_players(f"{self.peer.my_ip}:Bye")
+                    self.peer.send_msg_to_all_players("Bye")
                     self.peer.quit()
                     self.peer.stop_listen_event.set()
                     self.peer.stop_broadcast_event.set()
