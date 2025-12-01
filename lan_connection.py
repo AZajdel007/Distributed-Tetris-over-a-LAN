13fc71595da5010de8ec133b275457c1ee8c0c23
Antek <a.zajdel24@gmail.com>
2025-12-01 15:08:47 +0100

    mega ekperymentalne poprawki k-width

--- lan_connection.py
+++ lan_connection.py
@@ -118,10 +118,12 @@ class Peer:
                 print(f"Otrzymano: {data}:{sender}")
                 if sender[0] in self.known_peers:
                     msg = [data.decode(), sender]
-                    if '-' in msg[0].split(':')[1] and int(data.decode().split(':')[1].split('-')[0]) not in self.listen_ignore_list:
-                        if msg not in self.received_msg:
-                            self.received_msg.append(msg)
-                            print(self.received_msg)
+
+                    if '-' in msg[0]:
+                        if msg[0].split('-')[0] not in self.listen_ignore_list:
+                            if msg not in self.received_msg:
+                                self.received_msg.append(msg)
+                                print(self.received_msg)
                     else:
                         self.received_msg.append(msg)
                         print(self.received_msg)
