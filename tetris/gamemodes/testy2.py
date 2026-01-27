2ca0b20f06d32ca80540bf58748826e961282375
Antek <a.zajdel24@gmail.com>
2026-01-27 20:54:30 +0100

    testy

--- tetris/gamemodes/testy2.py
+++ tetris/gamemodes/testy2.py
@@ -41,6 +41,23 @@ class ShiftingTetris(g.Game):
                     if all(x == 1 for x in ready_players):
                         loop = False
 
+    def shift(self):
+        last_column = [row[-1] for row in self.grid.grid]
+        msg = ",".join(map(str, last_column))
+        self.peer.received_msg.clear()
+        not_ready_players = self.known_peers
+        while len(not_ready_players) != 0:
+            self.peer.send_msg_to_all_players("READY TO SHIFT!")
+            if len(self.peer.received_msg) != 0:
+                new_msg = self.peer.received_msg.pop()
+                msg, sender_ip = new_msg
+                if msg == "READY TO SHIFT!":
+                    if sender_ip in not_ready_players:
+                        not_ready_players.remove(sender_ip)
+
+        self.peer.send_msg_to_one_player(msg, self.next_player)
+
+
 
     def game_loop(self):
         print("Start!!!")
@@ -48,6 +65,7 @@ class ShiftingTetris(g.Game):
         listening_thread.start()
 
         block_goes_down = pg.USEREVENT + 1
+        shift_event = pg.USEREVENT + 2
 
         self.players_row_status = [[0 for n in range(len(self.peer.known_peers)+1)] for n in range(20)]
 
@@ -58,15 +76,96 @@ class ShiftingTetris(g.Game):
 
         # Ustawiamy timer co 1000 ms (czyli co 1 sekundę)
         pg.time.set_timer(block_goes_down, 1000)
-
-        #self.even_start()
-
+        pg.time.set_timer(shift_event, 15000)
+        self.even_start()
         self.peer.received_msg.clear()
-        self.peer.quit()
-        self.peer.stop_listen_event.set()
-        self.peer.stop_broadcast_event.set()
-        listening_thread.join()
-        del self.peer
+
+        clock_start = pg.time.get_ticks()
+        game_time_sec = 0
+        while self.loop:
+            if self.current_block.is_placed:
+                self.current_block = self.next_block
+                if not self.current_block.check_collision_with_wall(0, self.grid):
+                    for row in range(self.grid.rows - 1, -1, -1):
+                        tiles = 0
+                        for col in range(self.grid.cols):
+                            if self.grid.grid[row][col] != 0:
+                                tiles = tiles + 1
+                        if tiles == 10:
+                            del self.grid.grid[row]
+                            self.grid.grid.insert(0, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+                    self.next_block = self.random_new_block()
+                else:
+                    self.loop = False
+                    self.peer.send_msg_to_all_players("RIP")
+                    clock_end = pg.time.get_ticks()
+                    game_time_sec = (clock_end - clock_start) / 1000
+                    self.peer.quit()
+                    self.peer.stop_listen_event.set()
+                    self.peer.stop_broadcast_event.set()
+                    listening_thread.join()
+
+            if len(self.peer.received_msg) != 0:
+                print("elo")
+                new_msg = self.peer.received_msg.pop()
+                new_msg, sender_ip = new_msg
+                print(self.players_row_status)
+
+                if ',' in new_msg:
+                    col = new_msg.split(',')
+                    print(col)
+
+
+                elif "RIP" in new_msg:
+                    self.loop = False
+                    clock_end = pg.time.get_ticks()
+                    game_time_sec = (clock_end - clock_start) / 1000
+                    self.peer.quit()
+                    self.peer.stop_listen_event.set()
+                    self.peer.stop_broadcast_event.set()
+                    listening_thread.join()
+                    del self.peer
+                elif "Bye" in new_msg:
+                    self.loop = False
+                    clock_end = pg.time.get_ticks()
+                    game_time_sec = (clock_end - clock_start) / 1000
+                    self.peer.quit()
+                    self.peer.stop_listen_event.set()
+                    self.peer.stop_broadcast_event.set()
+                    listening_thread.join()
+                    del self.peer
+
+                print(self.players_row_status)
+
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    self.peer.send_msg_to_all_players("Bye")
+                    self.peer.quit()
+                    self.peer.stop_listen_event.set()
+                    self.peer.stop_broadcast_event.set()
+                    listening_thread.join()
+                    pg.quit()
+                    sys.exit()
+                elif event.type == block_goes_down:
+                    self.current_block.move_down(self.grid)
+                    if self.current_block.check_collision_under(self.grid):
+                        self.current_block.put_on_grid(self.grid)
+                elif event.type == shift_event:
+                    self.shift()
+                elif event.type == pg.KEYDOWN:
+                    if event.key == pg.K_LEFT:
+                        self.current_block.move_x(-1, self.grid)
+                    elif event.key == pg.K_RIGHT:
+                        self.current_block.move_x(1, self.grid)
+                    elif event.key == pg.K_UP:
+                        self.current_block.rotate(self.grid)
+                    elif event.key == pg.K_DOWN:
+                        self.current_block.move_down(self.grid)
+                    elif event.key == pg.K_SPACE:
+                        self.current_block.drop(self.grid)
+
+            self.draw()
+        self.game_over(self.screen, game_time_sec)
 
 
 def start_shifting_game(screen, bg_color, clock):
